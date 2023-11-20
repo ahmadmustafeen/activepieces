bd72aa6b0bb3c2fff24c2e0c09e4cf5d869813ee
Adnan Hussain <adnan@geeskofkolachi.com>
2023-11-20 13:40:56 +0500

    chore: minor refactor

---
+++ packages/backend/src/app/valuehub/constants/prompts.ts
@@ -0,0 +1,86 @@
+import { ChatCompletionRequestMessageRoleEnum } from 'openai'
+
+const COMPLEXITY_CALCULATION_MESSAGE = `
+For each generator tasks please calculate the complexity as:
+1. Everything that can be automated directly is equal to 1.
+2. Everything that can be automated with some input from the user is equal to 2.
+3. Everything that can be automated with some work from a freelancer is equal to 3.
+4. Everything that needs to be done by the user or can be outsourced is equal to 4.
+5. Everything that needs to be done by the user and cant be outsourced is equal to 5.
+
+Assign the complexity level in $complexity variable
+`
+
+export const PROMPT_MESSAGES = [
+    {
+        role: ChatCompletionRequestMessageRoleEnum.System,
+        content: `
+You are a experienced Project Manager, and you will be given goals, for each goal, you will return a JSON of tasks.
+All these task should be directing towards the main goal.
+
+
+Please provide atleast 5 tasks. 
+$goalId is a dynamic number value provided by the user. 
+Return value of complexity, colourTag and dueDate as empty string.
+Status should be "TODO" and type should be "BUG" and priority should be "3".
+Fill out title and description.
+
+${COMPLEXITY_CALCULATION_MESSAGE}
+
+Format for json would be as follow:
+
+<json>
+    [{
+        "title":"Social Media Marketing",
+        "description":"We will work on personal brand",
+        goalId:$goalId,
+        "priority":"",
+        "complexity":$complexity,
+        "status:"",
+        "type:"",
+        "colourTag":"",
+        "dueDate":""
+    }]
+</json>
+
+`,
+    },
+]
+
+export const REGENERATE_PROMPT_MESSAGES = [
+    {
+        role: ChatCompletionRequestMessageRoleEnum.System,
+        content: `
+You are a experienced Project Manager, and you will be given goals, for each goal, you will return a JSON of tasks.
+
+Tasks given in <tasks></tasks> xml are task previously created by user.
+
+All new task should be directing towards the main goal, keeping the task already created in mind.
+
+Please provide atleast 3 new tasks. 
+$goalId is a dynamic number value provided by the user. 
+Return value of complexity, colourTag and dueDate as empty string.
+Status should be "TODO" and type should be "BUG" and priority should be "3".
+Fill out title and description.
+
+${COMPLEXITY_CALCULATION_MESSAGE}
+
+Format for json would be as follow:
+
+<json>
+    [{
+        "title":"Social Media Marketing",
+        "description":"We will work on personal brand",
+        goalId:$goalId,
+        "priority":"",
+        "complexity":$complexity,
+        "status:"",
+        "type:"",
+        "colourTag":"",
+        "dueDate":""
+    }]
+<json>
+
+`,
+    },
+]
